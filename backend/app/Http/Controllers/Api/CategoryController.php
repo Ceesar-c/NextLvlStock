a3206cb03d72c31a7fb6aa8829be1229473e3a1c
Cesar Castellanos <dsdfafdas@hotmail.com>
2026-07-29 10:04:05 -0500

    feat: complete category CRUD operations

--- backend/app/Http/Controllers/Api/CategoryController.php
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use Illuminate\Http\Request;
 use App\Models\Category;
 
@@ -35,24 +36,46 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return response()->json([
+            'data' => $category
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $data = $request->validated();
+        $category->update($data);
+
+        return response()->json([
+            'message' => 'La categoría  ha sido actualizada correctamente.',
+            'data' => $category,
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        if(!$category->is_active){
+            return response()->json([
+                'message' => 'La categoría ya se encuentra inactiva.',
+                'data' => $category,
+            ], 200);
+        }
+
+        $category->update([
+            'is_active' => false
+        ]);
+
+        return response()->json([
+            'message' => 'La categoría ha sido inactivada correctamente.',
+            'data' => $category,
+        ], 200);
     }
 }
