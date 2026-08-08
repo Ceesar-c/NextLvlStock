dbab40d9c2fd7abb2041599286dd8492d2ef43b5
Cesar Castellanos <dsdfafdas@hotmail.com>
2026-08-07 20:14:16 -0500

    feat: implement customers module

---
+++ backend/app/Models/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_type',
+        'name',
+        'identification',
+        'phone',
+        'email',
+        'address',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+}
