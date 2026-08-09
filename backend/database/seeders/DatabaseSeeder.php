4cfd86dbb141b954633e3fe6ea8f24db49921345
Cesar Castellanos <dsdfafdas@hotmail.com>
2026-08-09 12:12:21 -0500

    feat: implement authentication module

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@nextlvlstock.test',
+            'password' => 'password',
+            'is_active' => true,
         ]);
     }
 }
